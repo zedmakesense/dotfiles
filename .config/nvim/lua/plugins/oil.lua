38e42365ec82c1ca3d058f9734cfc059bc0501bf
zedonarch <zedonix@proton.me>
2025-08-08 09:11:52 +0530

    oil.nvim

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+        { '-', '<cmd>Oil<cr>', desc = 'Grug Find & Replace' },
+    },
+}
