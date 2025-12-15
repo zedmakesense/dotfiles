150efdbb26bc430b504f88d6695bf12155b3ae34
zedonarch <zedonix@proton.me>
2025-12-15 09:51:59 +0530

    gimp is to copy; nvim dropdown toggle

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -63,7 +63,7 @@ return {
             { 'j-hui/fidget.nvim', opts = {} },
         },
         config = function()
-            -- local capabilities = require('blink.cmp').get_lsp_capabilities()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
 
             local lsps = {
                 'jsonls',
@@ -215,68 +215,71 @@ return {
             },
         },
     },
-    -- {
-    --     {
-    --         'saghen/blink.cmp',
-    --         version = '1.*',
-    --         event = { 'InsertEnter', 'CmdlineEnter' },
-    --         dependencies = {
-    --             {
-    --                 'L3MON4D3/LuaSnip',
-    --                 version = '2.*',
-    --                 build = (function()
-    --                     -- Build Step is needed for regex support in snippets.
-    --                     -- This step is not supported in many windows environments.
-    --                     -- Remove the below condition to re-enable on windows.
-    --                     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-    --                         return
-    --                     end
-    --                     return 'make install_jsregexp'
-    --                 end)(),
-    --                 dependencies = {
-    --                     {
-    --                         'rafamadriz/friendly-snippets',
-    --                         config = function()
-    --                             require('luasnip.loaders.from_vscode').lazy_load()
-    --                         end,
-    --                     },
-    --                 },
-    --                 opts = {},
-    --             },
-    --             'onsails/lspkind.nvim',
-    --             'folke/lazydev.nvim',
-    --         },
-    --         ---@module 'blink.cmp'
-    --         ---@type blink.cmp.Config
-    --         opts = {
-    --             keymap = {
-    --                 preset = 'default',
-    --             },
-    --             appearance = {
-    --                 nerd_font_variant = 'mono',
-    --             },
-    --             completion = {
-    --                 list = {
-    --                     selection = {
-    --                         preselect = true,
-    --                         auto_insert = true,
-    --                     },
-    --                 },
-    --                 documentation = {
-    --                     auto_show = false,
-    --                     auto_show_delay_ms = 500,
-    --                 },
-    --             },
-    --             sources = {
-    --                 default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
-    --                 providers = {
-    --                     lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-    --                 },
-    --             },
-    --             snippets = { preset = 'luasnip' },
-    --             signature = { enabled = true },
-    --             fuzzy = { implementation = 'prefer_rust_with_warning' },
-    --         },
-    --     },
-    -- },
+    {
+        {
+            'saghen/blink.cmp',
+            version = '1.*',
+            event = { 'InsertEnter', 'CmdlineEnter' },
+            dependencies = {
+                {
+                    'L3MON4D3/LuaSnip',
+                    version = '2.*',
+                    build = (function()
+                        -- Build Step is needed for regex support in snippets.
+                        -- This step is not supported in many windows environments.
+                        -- Remove the below condition to re-enable on windows.
+                        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                            return
+                        end
+                        return 'make install_jsregexp'
+                    end)(),
+                    dependencies = {
+                        {
+                            'rafamadriz/friendly-snippets',
+                            config = function()
+                                require('luasnip.loaders.from_vscode').lazy_load()
+                            end,
+                        },
+                    },
+                    opts = {},
+                },
+                'onsails/lspkind.nvim',
+                'folke/lazydev.nvim',
+            },
+            ---@module 'blink.cmp'
+            ---@type blink.cmp.Config
+            opts = {
+                keymap = {
+                    preset = 'default',
+                },
+                appearance = {
+                    nerd_font_variant = 'mono',
+                },
+                completion = {
+                    menu = {
+                        enabled = false,
+                    },
+                    list = {
+                        selection = {
+                            preselect = true,
+                            auto_insert = true,
+                        },
+                    },
+                    documentation = {
+                        auto_show = false,
+                        auto_show_delay_ms = 500,
+                    },
+                },
+                sources = {
+                    default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+                    providers = {
+                        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+                    },
+                },
+                snippets = { preset = 'luasnip' },
+                signature = { enabled = true },
+                fuzzy = { implementation = 'prefer_rust_with_warning' },
+            },
+        },
+    },
 }
