8b9e8a7cb99d61c170e75b6cd21e3fb87272dbee
zedonarch <zedonix@proton.me>
2026-02-07 10:08:51 +0530

    fix tmux not restoring paths

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -22,6 +22,8 @@ vim.g.netrw_browse_split = 4
 vim.g.netrw_altv = 1
 vim.g.netrw_liststyle = 3
 
+vim.opt.autochdir = false
+
 -- UI Configuration
 vim.g.have_nerd_font = true
 vim.opt.termguicolors = true
