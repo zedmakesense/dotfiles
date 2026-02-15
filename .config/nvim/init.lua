511817804e00bcdab7fc7f88a3748252b8eb469e
zedonarch <zedonix@proton.me>
2026-02-15 09:23:36 +0530

    autocmd for previous line nvim; bookmark

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -135,3 +135,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
   callback = function() vim.cmd [[%s/\s\+$//e]] end,
 })
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+    group = autocmd,
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
+
